bf7c6e267dda88cfedc1a2c7f94c33d34e157eff
Jonathan Lo <72477459+jonathanlo411@users.noreply.github.com>
2022-09-14 16:12:41 -0700

    Update talentday.js

--- js/talentday.js
+++ js/talentday.js
@@ -3,7 +3,6 @@
 var students = document.getElementById('bt-students')
 var employers = document.getElementById('bt-employers')
 
-addRedirect(students, 'https://www.google.com/')
 addRedirect(employers, 'talent-day-2022-geisel.pdf')
 
 function addRedirect(bt, site) {
